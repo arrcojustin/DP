5ed6d8911b7b3e072c0b9413b04b59e35db13fa3
arrco-justin <bilibilijusstin123@gmail.com>
2025-05-04 22:26:25 +0800

    添加项目文件。

---
+++ OftenTest/OftenTest.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include <string>
+using namespace std;
+int m, n, b[10], temp;
+int a[30];
+int main() {
+    cin >> m >> n;
+    temp = 0;
+    for (int i = m; i <= n;i++) {
+        a[temp] = i;
+        temp++;
+    }
+    for (int i = 0; i <= temp - 1;i++) {
+        std::string s_num = std::to_string(a[i]);
+        for (int c : s_num) {
+            b[c - 48]++;
+        }
+    }
+    for (int i = 0; i <= 9;i++) {
+        cout << b[i] << ' ';
+    }
+}
